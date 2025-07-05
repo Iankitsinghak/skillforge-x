4e5618d03ca6da4c0941557e112897732cd768ca
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-05 17:08:28 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile";
-import Challenges from "./pages/Challenges";
-import Collab from "./pages/Collab";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+import Challenges from './pages/Challenges';
+import Collab from './pages/Collab';
+import Sidebar from './layout/Sidebar';
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/challenges" element={<Challenges />} />
-        <Route path="/collab" element={<Collab />} />
-      </Routes>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 p-4">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/challenges" element={<Challenges />} />
+            <Route path="/collab" element={<Collab />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
