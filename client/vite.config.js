924b107c315d09d675126a104ec5f731c740f157
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-07 00:16:47 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
-// client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: '.', // root is client/
+  root: '.',
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  }
+  },
 });
