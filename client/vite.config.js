2e5284da8c24ea453d303073bc7656543fcf8155
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-09 19:57:59 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,9 @@
-// /client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: '.',     // start build from /client
-  base: './',    // ensures relative paths for Vercel
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-  },
+  }
 });
