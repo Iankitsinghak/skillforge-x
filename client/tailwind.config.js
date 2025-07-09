46ef193722d0d08d1640874f088266bc145e9743
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-09 20:22:22 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,22 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  darkMode: "class",
-  theme: { extend: {} },
-  plugins: [],
-};
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", 
+  ],
+  darkMode: "class", 
+  theme: {
+    extend: {
+      
+      colors: {
+        primary: "#3B82F6", 
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"), 
+    require("@tailwindcss/typography"), 
+  ],
+  corePlugins: {
+    preflight: true, 
+  },
+}
