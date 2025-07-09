fcd70a18d8ca87deb4f2039c762d1eff38bc7150
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-09 19:57:39 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,6 @@
-// /client/tailwind.config.js
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  darkMode: "class",
+  theme: { extend: {} },
   plugins: [],
 };
