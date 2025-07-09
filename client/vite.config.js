23cb17408abc91cb7836628bf167081c23ffaec7
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-09 20:29:57 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,17 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',      
-    emptyOutDir: true,    
-    sourcemap: true,      
-    chunkSizeWarningLimit: 1600, 
+    outDir: './dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    chunkSizeWarningLimit: 1600,
   },
-  server: {
-    port: 3000,          
-    open: true,          
-  },
-  preview: {
-    port: 3000,          
-  },
-  base: '/',            
+  base: './', 
+  publicDir: './public',
 });
