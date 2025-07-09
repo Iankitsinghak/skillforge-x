fcc447382f3d695c9cdad4812e4e294802166029
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-09 20:30:43 +0530

    Update postcss.config.js

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,17 +1,6 @@
 export default {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {}, 
     tailwindcss: {},
-    autoprefixer: {
-      flexbox: 'no-2009', 
-    },
-    ...(process.env.NODE_ENV === 'production'
-      ? {
-          cssnano: { 
-            preset: 'default',
-          },
-        }
-      : {}),
+    autoprefixer: {},
   },
 };
