5693fcefe1d7105678b5219460bca7c46987d2cf
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-10 22:54:06 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: './dist',
-    emptyOutDir: true,
-    sourcemap: true,
-    chunkSizeWarningLimit: 1600,
-  },
-  base: './', 
-  publicDir: './public',
-});
+})
