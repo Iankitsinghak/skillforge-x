44d6c55225a791d3738114e1db5a283d065cbc99
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-16 22:09:13 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to DB
+connectDB();
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+
+app.get('/', (req, res) => res.send('SkillForge X API is running'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
