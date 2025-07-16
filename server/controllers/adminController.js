6ecd80a65532c2993e2f69b24d789901f8850fcb
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-17 01:03:32 +0530

    Update adminController.js

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -9,3 +9,21 @@ exports.postChallenge = async (req, res) => {
     res.status(500).json({ msg: 'Failed to post challenge' });
   }
 };
+const Job = require('../models/Job');
+
+exports.postJob = async (req, res) => {
+  try {
+    const { title, description, requiredSkills, company, isStartup } = req.body;
+    const job = await Job.create({
+      title,
+      description,
+      requiredSkills,
+      company,
+      isStartup,
+      postedBy: req.user.id
+    });
+    res.status(201).json({ msg: 'Job posted successfully', job });
+  } catch (err) {
+    res.status(500).json({ msg: 'Failed to post job' });
+  }
+};
