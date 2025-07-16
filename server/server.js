15028d37e53fbab4d3731249d726a8eb9b47ade8
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-17 00:16:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,3 +21,4 @@ app.get('/', (req, res) => res.send('SkillForge X API is running'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.use('/api/projects', require('./routes/projectRoutes'));
+app.use('/api/jobs', require('./routes/jobRoutes'));
