e0d5b311e0d2316af8c4c344fc8806545d88889a
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-17 19:07:39 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
+  root: '.', // this is already implied
   build: {
     outDir: 'dist',
-  }
+    emptyOutDir: true
+  },
+  server: {
+    port: 5173
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
