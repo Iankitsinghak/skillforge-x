c69e37cc8dba7e955eb54ea03b1eb2b0c5821bff
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-18 02:26:10 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage'; // 👈 import landing page
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
@@ -12,91 +13,48 @@ import ChallengeDetail from './pages/ChallengeDetail';
 import PostChallenge from './pages/PostChallenge';
 import PostJob from './pages/PostJob';
 import AskAI from './pages/AskAI';
-import Navbar from './components/Navbar';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <Router>
+      {/* ✅ Navbar should be outside Routes to be global */}
       <Navbar />
       <Routes>
-        {/* 👇 Public landing page */}
+        {/* Public Routes */}
         <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
 
-        {/* 👇 Private routes */}
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/projects"
-          element={
-            <PrivateRoute>
-              <ProjectFeed />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/jobs"
-          element={
-            <PrivateRoute>
-              <JobFeed />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/jobs/startups"
-          element={
-            <PrivateRoute>
-              <StartupJobs />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/challenges"
-          element={
-            <PrivateRoute>
-              <Challenges />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/challenges/:id"
-          element={
-            <PrivateRoute>
-              <ChallengeDetail />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/admin/challenges"
-          element={
-            <PrivateRoute>
-              <PostChallenge />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/admin/jobs"
-          element={
-            <PrivateRoute>
-              <PostJob />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/ask"
-          element={
-            <PrivateRoute>
-              <AskAI />
-            </PrivateRoute>
-          }
-        />
+        {/* Protected Routes */}
+        <Route path="/dashboard" element={
+          <PrivateRoute><Dashboard /></PrivateRoute>
+        } />
+        <Route path="/projects" element={
+          <PrivateRoute><ProjectFeed /></PrivateRoute>
+        } />
+        <Route path="/jobs" element={
+          <PrivateRoute><JobFeed /></PrivateRoute>
+        } />
+        <Route path="/jobs/startups" element={
+          <PrivateRoute><StartupJobs /></PrivateRoute>
+        } />
+        <Route path="/challenges" element={
+          <PrivateRoute><Challenges /></PrivateRoute>
+        } />
+        <Route path="/challenges/:id" element={
+          <PrivateRoute><ChallengeDetail /></PrivateRoute>
+        } />
+        <Route path="/admin/challenges" element={
+          <PrivateRoute><PostChallenge /></PrivateRoute>
+        } />
+        <Route path="/admin/jobs" element={
+          <PrivateRoute><PostJob /></PrivateRoute>
+        } />
+        <Route path="/ask" element={
+          <PrivateRoute><AskAI /></PrivateRoute>
+        } />
       </Routes>
     </Router>
   );
