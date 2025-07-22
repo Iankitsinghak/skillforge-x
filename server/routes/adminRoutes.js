b6150b6e0e26c3fa801bd0f610f663acd2de7b16
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-23 03:47:32 +0530

    Update adminRoutes.js

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const { postChallenge } = require('../controllers/adminController');
-const protect = require('../middleware/authMiddleware'); // basic JWT protect
-const { postJob } = require('../controllers/adminController');
+const protect = require('../middleware/authMiddleware');
 
+const {
+  postJob,
+  postProject,
+  postChallenge,
+  getJobApplicants,
+  getProjectRequests
+} = require('../controllers/adminController');
+
+// POST a new job
 router.post('/jobs', protect, postJob);
 
+// POST a new project
+router.post('/projects', protect, postProject);
+
+// POST a new challenge
 router.post('/challenges', protect, postChallenge);
+
+// GET job applicants
+router.get('/job-applicants', protect, getJobApplicants);
+
+// GET project join requests
+router.get('/project-requests', protect, getProjectRequests);
 
 module.exports = router;
