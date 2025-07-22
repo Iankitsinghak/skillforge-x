b0f03098780e6322060ae6cf6d1959ffa38289f1
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-23 03:38:45 +0530

    Create Application.js

---
+++ server/models/Application.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const applicationSchema = new mongoose.Schema({
+  job: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Job',
+    required: true,
+  },
+  applicant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  appliedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'selected', 'rejected'],
+    default: 'pending',
+  },
+});
+
+module.exports = mongoose.model('Application', applicationSchema);
