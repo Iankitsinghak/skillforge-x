995b652f90460c4a8982b377d6bc341ea9de81c6
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-23 02:58:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const geminiRoutes = require('./routes/geminiRoutes');
 
 dotenv.config();
 const app = express();
@@ -21,6 +22,7 @@ app.use('/api/challenges', require('./routes/challengeRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
 app.use('/api/ask', require('./routes/geminiRoutes'));
 app.use('/api/user', require('./routes/userRoutes'));
+app.use('/api/gemini', geminiRoutes);
 
 app.get('/', (req, res) => res.send('SkillForge X API is running'));
 
