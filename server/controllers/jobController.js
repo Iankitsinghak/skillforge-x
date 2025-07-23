55083400390447ab4a09aabfa4a2222dc47b345c
Ankit Singh <ankitsinghak9051@gmail.com>
2025-07-23 17:48:59 +0530

    Update jobController.js

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -23,6 +23,15 @@ exports.getStartupJobs = async (req, res) => {
   }
 };
 
+exports.getAllJobs = async (req, res) => {
+  try {
+    const jobs = await Job.find().sort({ createdAt: -1 }); // newest first
+    res.status(200).json(jobs);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 // ✅ Apply to Job
 exports.applyToJob = async (req, res) => {
   try {
